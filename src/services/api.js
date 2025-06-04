a200e1676d1b034a5ac820b711df506a373eac6f
Eva Frankovic <evafrankovic7@example.com>
2025-06-04 19:34:50 -0400

    Pushing final project

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "7a6eb31281c2d714115fb671c0d0961b";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
